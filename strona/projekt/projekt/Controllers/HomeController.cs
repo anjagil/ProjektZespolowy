32ccc1775d8be62191fb7f3897aaf06b1521a6db
saganeczka <sagan.beata@outlook.com>
2016-04-25 18:00:15 +0200

    formularz, o nas, zmiana hasla

--- strona/projekt/projekt/Controllers/HomeController.cs
+++ strona/projekt/projekt/Controllers/HomeController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using projekt.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+
+
 
 namespace projekt.Controllers
 {
@@ -24,6 +30,43 @@ namespace projekt.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("kontakt.air.wrobel@outlook.com"));  // replace with valid value 
+                message.From = new MailAddress("kontakt.air.wrobel@outlook.com");  // replace with valid value
+                message.Subject = "Wiadomość od użytkownika strony AirWrobel";
+                message.Body = string.Format(body, model.Name, model.EmailAdress, model.Text);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "kontakt.air.wrobel@outlook.com",  // replace with valid value
+                        Password = "ProjektZespolowy2016"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    //smtp.UseDefaultCredentials = false;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+
+            }
+            return View(model);
+        }
+        public ActionResult Sent()
+        {
             return View();
         }
     }
---
+++ strona/projekt/projekt/Models/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace projekt.Models
+{
+    public class ContactViewModel
+    {
+        [Required, Display(Name = "Imię")]
+        public string Name { get; set; }
+        [Required, Display(Name = "Email"), EmailAddress]
+        public string EmailAdress { get; set; }
+        [Required, Display(Name="Twoja wiadomość")]
+        public string Text { get; set; }
+    }
+
+
+}
